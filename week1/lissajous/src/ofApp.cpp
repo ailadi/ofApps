275fffdad62fdee483c60b7e7b3b6c32b664da98
ailadi <ailadi@Ailadi.home>
2018-03-05 22:38:40 -0500

    more exercises lissajous, whitney

--- week1/lissajous/src/ofApp.cpp
+++ week1/lissajous/src/ofApp.cpp
@@ -2,43 +2,54 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    ofBackground(255);
     
+    ofBackground(0);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    
-   
-  
-}
 
-void ofApp::tri(float x, float y, float r, float angle){
-    
-    ofPoint pt[3];
-    for (int i=0; i<3; i++){
-        pt[i].x = x + cos(TWO_PI/3*i +angle)*r;
-        pt[i].y = y + sin(TWO_PI/3*i +angle)*r;
-        ofSetColor(0);
-   }
-    ofDrawTriangle(pt[0], pt[1], pt[2]);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
- 
-    //float sint = ofMap(sin)
     
-    for (int j=0; j<280; j+=8){
-        for (int i=0; i<400; i+=10){
-            float t = ofGetElapsedTimef();
-            float radius = ofMap(sin(t*i/100+j/100), -1, 1, 100, 400);
-            ptx = xcenter + cos(t*j/200)*radius*i/100*2;
-            pty = ycenter + sin(t*j/200)*radius*i/100*2;
-            tri(ptx, pty, 10, sin(t+i)*8);
-        //ofDrawCircle(xcenter,ycenter,10);
-        }
+    
+    ofPoint pt;
+    float t = ofGetElapsedTimef();
+    
+    //parameters
+    //float A = ofMap(sin(t*4), -1,1, 0, 400); // horizontal Amplitude
+    //float B = ofMap(sin(t*4), -1,1, 0, 400); // vertical Amplitude
+    float A = 400;
+    float B = 400;
+    
+    float a = 3; // horizontal Frequency
+    float b = 2; // vertical Frequency
+    
+    //float phaseDelta = ofMap(sin(t*4), -1,1,0,1);
+    float phase = TWO_PI/4;
+   
+    //center of my gfx
+    center.x = 500;
+    center.y = 500;
+    
+    pt.x = center.x + A*sin(a*t + phase);
+    pt.y = center.y + B*sin(b*t);
+
+    //draw
+    ofSetColor(255);
+    
+    ofDrawCircle(pt , 10);
+    
+    //draw line
+    line.addVertex(pt.x, pt.y);
+    if(line.size() > 1000){
+        line.getVertices().erase(line.getVertices().begin());
     }
+    line.draw();
+    
+    
 }
 
 //--------------------------------------------------------------
